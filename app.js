a74c9ad08beb7701874c0fc8ecdab5de82343b35
karthikguna2996 <141558011+karthikguna2996@users.noreply.github.com>
2023-10-14 21:33:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let express = require("express");
 let app = express();
 let { open } = require("sqlite");
-app.listen(3000, () => {
-  console.log("server ON");
-});
 let sqlite3 = require("sqlite3");
 let path = require("path");
-let dbPath = path.join(__dirname, cricketTeam.db);
+let dbPath = path.join(__dirname, "cricketTeam.db");
 let db = null;
 let connectDatabase = async () => {
   try {
     db = await open({
       filename: dbPath,
-      drive: sqlite3.Database,
+      driver: sqlite3.Database,
+    });
+    app.listen(3003, () => {
+      console.log("server started");
     });
   } catch (err) {
     console.log(`there is an ${err.message}`);
@@ -23,9 +23,25 @@ connectDatabase();
 app.get("/players/", async (request, response) => {
   const query = `
          SELECT *
-         FROM cricket_team`;
+         FROM cricket_team;`;
   const playersList = await db.all(query);
-  response.send(playersList);
   console.log(playersList);
+  let ans = {
+    playerId: playersList.player_id,
+    playerName: playersList.player_name,
+    jerseyNumber: playersList.jersey_number,
+    role: playersList.role,
+  };
+  let playerNameList = playersList.map((val) => {
+    let ans = {
+      playerId: val.player_id,
+      playerName: val.player_name,
+      jerseyNumber: val.jersey_number,
+      role: val.role,
+    };
+    return ans;
+  });
+  response.send(playerNameList);
+  console.log(playerNameList);
 });
 modules.exports = app;
