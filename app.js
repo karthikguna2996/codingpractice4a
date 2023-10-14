0aa8b6cfd769735bc50da75ff25158c7c7f38167
karthikguna2996 <141558011+karthikguna2996@users.noreply.github.com>
2023-10-14 23:15:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
-app.use(express.json())
+app.use(express.json());
 let { open } = require("sqlite");
 let sqlite3 = require("sqlite3");
 let path = require("path");
@@ -46,10 +46,10 @@ app.get("/players/", async (request, response) => {
   response.send(playerNameList);
   console.log(playerNameList);
 });
-app.post("/players/", async (request,response) =>{
-    let addPlayerDetails = request.body;
-    let {playerName,jerseyNumber,role} = addPlayerDetails;
-    let addQuery ='
+app.post("/players/", async (request, response) => {
+  let addPlayerDetails = request.body;
+  let { playerName, jerseyNumber, role } = addPlayerDetails;
+  let addQuery = `
               INSERT INTO
                  cricket_team(player_name,jersey_number,role)
               VALUES
@@ -58,57 +58,50 @@ app.post("/players/", async (request,response) =>{
                   ${jerseyNumber},
                   ${role}
               );
-    ';
-    await db.run(addQuery);
-    response.send("Player Added to Team")
+    `;
+  await db.run(addQuery);
+  response.send("Player Added to Team");
+});
 
-})
-
- 
-
-app.get("/players/:playerId/",async (request,response)=>{
-                let {playerId} = request.params;
-                let idQuery = '
+app.get("/players/:playerId/", async (request, response) => {
+  let { playerId } = request.params;
+  let idQuery = `
                 SELECT *
                 FROM cricket_team
-                WHERE player_id = ${playerId};';
-                let playerDetails = await db.get(idQuery);
-                let singlePlayerDetail ={
-                    playerId: playerDetails.player_id,
-                    playerName: playerDetails.player_name,
-                    jerseyNumber: playerDetails.jersey_number,
-                    role: playerDetails.role,
-                }
-                response.send(singlePlayerDetail)
-})
+                WHERE player_id = ${playerId};`;
+  let playerDetails = await db.get(idQuery);
+  let singlePlayerDetail = {
+    playerId: playerDetails.player_id,
+    playerName: playerDetails.player_name,
+    jerseyNumber: playerDetails.jersey_number,
+    role: playerDetails.role,
+  };
+  response.send(singlePlayerDetail);
+});
 
-app.put("/players/:playerId/", async (request,response)=>{
-     let {playerId} = request.params;
-     let putPlayerDetails = request.body;
-     let {playerName,jerseyNumber,role} = putPlayerDetails;
-     let putQuery = `UPDATE 
+app.put("/players/:playerId/", async (request, response) => {
+  let { playerId } = request.params;
+  let putPlayerDetails = request.body;
+  let { playerName, jerseyNumber, role } = putPlayerDetails;
+  let putQuery = `UPDATE 
                           cricket_team
                         SET 
                            player_name = ${playerName},
                            jersey_number = ${jerseyNumber},
                            role = ${role}
                         WHERE
-                           player_id = ${playerId};`
-    await db.run(putQuery);
-    response.send("Player Details Updated");
-}
-)
+                           player_id = ${playerId};`;
+  await db.run(putQuery);
+  response.send("Player Details Updated");
+});
 
-app.delete("/players/:playerId/",async (request,response)=>{
-   let {playerId} = request.params;
-   let deleteQuery = `
+app.delete("/players/:playerId/", async (request, response) => {
+  let { playerId } = request.params;
+  let deleteQuery = `
      DELETE FROM cricket_team
       WHERE player_id = ${playerId};`;
-    await db.run(deleteQuery);
-    response.send("Player Removed");
-})
+  await db.run(deleteQuery);
+  response.send("Player Removed");
+});
 
 module.exports = app;
-
-
-
